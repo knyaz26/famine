185bc38c159715f3189f167eadc2a71b937e7d22
knyaz <saba2006.diasamidze@gmail.com>
2025-06-22 15:14:57 +0400

    set a few params for dashboard

--- ui/dashboard.py
+++ ui/dashboard.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import threading
 import sqlite3
 from graphic.game import Game
@@ -9,9 +10,23 @@ class Dashboard():
     
     def enter(self):
         self.root = tk.Tk()
+        self.root.title("Dashboard")
+        self.root.geometry("1024x600")
+        self.root.resizable(False, False)
         
         game = Game()
         threading.Thread(target=game.run, daemon=True).start()
+
+        self.notebook = ttk.Notebook(self.root)
+
+        self.info_tab = tk.Frame(self.notebook)
+
+        self.notebook.add(self.info_tab, text="Info")
+
+        self.notebook.pack(expand=True, fill="both")
+
+        self.info_text = tk.Label(self.info_tab, text="hehehe")
+        self.info_text.pack()
 
         self.root.after(100, self.update)
         self.root.mainloop()
