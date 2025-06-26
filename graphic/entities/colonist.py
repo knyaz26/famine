232e8fa3123365747ba0e4de04648834478c4c68
knyaz <saba2006.diasamidze@gmail.com>
2025-06-26 11:20:24 +0400

    colonists now move to food

--- graphic/entities/colonist.py
+++ graphic/entities/colonist.py
@@ -12,11 +12,11 @@ class Colonist():
         self.src = pr.Rectangle(0, 0, self.sprite.width, self.sprite.height)
         self.origin = pr.Vector2(self.sprite.width // 2, self.sprite.height)
         self.target = None
+        self.speed = 0.5
 
     def update(self):
-        self.rect = pr.Rectangle(self.pos_x, self.pos_y, self.sprite.width * 2, self.sprite.height * 2)
-        print(self.target)
-        
+        self.move_towards()
+        self.update_rectangle()
 
     def draw(self):
         pr.draw_texture_pro(
@@ -27,4 +27,22 @@ class Colonist():
             0,
             pr.WHITE
         )
-        
+
+    def move_towards(self):
+        if self.target:
+            print("moving toward", self.target.pos_x, self.target.pos_y)
+            
+            pos1 = (self.pos_x, self.pos_y)
+            pos2 = (self.target.pos_x, self.target.pos_y)
+            dx = pos2[0] - pos1[0]
+            dy = pos2[1] - pos1[1]
+            dist = (dx**2 + dy**2) ** 0.5
+            if dist==0:
+                return
+            nx, ny = dx / dist, dy / dist
+            move_dist = min(self.speed, dist)
+            self.pos_x = pos1[0] + nx * move_dist
+            self.pos_y = pos1[1] + ny * move_dist
+
+    def update_rectangle(self):
+        self.rect = pr.Rectangle(self.pos_x, self.pos_y, self.sprite.width * 2, self.sprite.height * 2)
